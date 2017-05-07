27e7ea5dd69b264f851dae9a4fe340cc736e9ebe
Wojciech Sankowski <sankowski.wojciech@gmail.com>
2017-05-07 14:37:18 +0200

    added cell implementation

---
+++ src/main/java/logic/Cell.java
@@ -0,0 +1,52 @@
+package logic;
+
+public class Cell {
+    private Coordinates coordinates;
+    private State state;
+
+    public Cell(Coordinates coordinates, State state) {
+        this.coordinates = coordinates;
+        this.state = state;
+    }
+
+    public Cell(Coordinates coordinates) {
+        this.coordinates = coordinates;
+        this.state = State.EMPTY;
+    }
+
+    public Coordinates getCoordinates() {
+        return coordinates;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return new String("[" + coordinates + ": " + state + "]");
+    }
+
+    @Override
+    public boolean equals(Object cell) {
+        if ( cell == null ) return false;
+        if ( this == cell ) return true;
+        if ( cell.getClass() != Cell.class ) return false;
+
+        Cell otherCell = (Cell) cell;
+
+        if( this.coordinates.equals(otherCell.coordinates) && this.state.equals(otherCell.state))
+            return true;
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return (coordinates.hashCode() + state.hashCode() + 31) * 5;
+    }
+
+}
