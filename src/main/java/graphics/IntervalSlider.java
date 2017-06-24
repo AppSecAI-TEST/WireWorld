8f18047462794cb30bf1be2354da4734346c8ebb
Wojciech Sankowski <sankowski.wojciech@gmail.com>
2017-06-24 10:06:35 +0200

    delay slider implementation

--- src/main/java/graphics/IntervalSlider.java
+++ src/main/java/graphics/IntervalSlider.java
@@ -2,16 +2,16 @@ package graphics;
 
 import javafx.scene.control.Slider;
 
+/*slider wyznaczajacy opoznienie pomiedzy kolejnymi wyswietleniami*/
 class IntervalSlider {
-    private Slider intervalSlider;
-
-    Slider makeIntervalSlider(int min, int max, int value) {
-        intervalSlider = new Slider(min, max, value);
+    static Slider makeIntervalSlider(int min, int max, int value) {
+        Slider intervalSlider = new Slider(min, max, value);
         intervalSlider.setShowTickLabels(true);
         intervalSlider.setShowTickMarks(true);
         intervalSlider.setMinWidth(200);
         intervalSlider.setMinorTickCount(3);
         intervalSlider.setMajorTickUnit(2);
+        intervalSlider.setSnapToTicks(true);
         return intervalSlider;
     }
 }
