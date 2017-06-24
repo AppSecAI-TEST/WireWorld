52abfef2b85d5acc86cd3d181493bb98606af444
Wojciech Sankowski <sankowski.wojciech@gmail.com>
2017-06-24 11:00:39 +0200

    generation algorithm interface

--- src/main/java/logic/GenerationAlgorithm.java
+++ src/main/java/logic/GenerationAlgorithm.java
@@ -2,6 +2,7 @@ package logic;
 
 import java.util.HashMap;
 
+/*interfejs dla algorytmu generowania kolejnych komorek*/
 public interface GenerationAlgorithm {
     public HashMap<Coordinates, Cell> getNextGeneration(HashMap<Coordinates, Cell> cellHashMap);
 }
