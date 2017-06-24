0767b1cae96c875b7ebd985477e007eb7f6d551a
Wojciech Sankowski <sankowski.wojciech@gmail.com>
2017-06-24 10:08:13 +0200

    view of delay slider

--- src/main/java/graphics/GenerationInterval.java
+++ src/main/java/graphics/GenerationInterval.java
@@ -1,36 +1,29 @@
 package graphics;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 
-class GenerationInterval {
+/*klasa stanowiaca opakowanie dla slidera, ktory wyznacza czas opoznienia*/
+    class GenerationInterval {
     private HBox generationInterval;
     private Slider intervalSlider;
     private Label captionLabel;
     private Label valueLabel;
 
-    void makeGenerationInterval(int min, int max, int value) {
-        generationInterval = new HBox(20);
-        intervalSlider = new IntervalSlider().makeIntervalSlider(min, max, value);
-        captionLabel = new Label("Interwał");
-        valueLabel = new Label(Double.toString(intervalSlider.getValue()));
-
-        intervalSlider.valueProperty().addListener(new ChangeListener<Number>() {
-            public void changed(ObservableValue<? extends Number> observableValue,
-                                Number oldValue, Number newValue) {
-                valueLabel.setText(String.format("%.2f", newValue));
-            }
-        });
-
+    public GenerationInterval(int min, int max, int value) {
+        generationInterval = new HBox(10);
+        intervalSlider = IntervalSlider.makeIntervalSlider(min, max, value);
+        captionLabel = new Label("Interwal");
+        valueLabel = new Label("[s]");
         generationInterval.getChildren().addAll(captionLabel, intervalSlider, valueLabel);
-//        return generationInterval;
     }
 
     HBox getGenerationInterval() {
         return generationInterval;
     }
 
+    Slider getIntervalSlider() {
+        return intervalSlider;
+    }
 }
