2110890bf8cc299e7831bd25d244ded925d8e4df
SankowskiWojciech <sankowski.wojciech@gmail.com>
2017-06-24 10:59:27 +0200

    Delete GenerationAlgorithm.java

--- src/main/java/logic/GenerationAlgorithm.java
+++
@@ -1,7 +0,0 @@
-package logic;
-
-import java.util.HashMap;
-
-public interface GenerationAlgorithm {
-    public HashMap<Coordinates, Cell> getNextGeneration(HashMap<Coordinates, Cell> cellHashMap);
-}
