70a6f6fcf45dfd1f6b6b597df62c253bb2f8527d
Wojciech Sankowski <sankowski.wojciech@gmail.com>
2017-06-24 10:14:59 +0200

    main stage

--- src/main/java/graphics/MainStage.java
+++ src/main/java/graphics/MainStage.java
@@ -3,21 +3,17 @@ package graphics;
 import javafx.stage.Stage;
 
 public class MainStage{
+
     private Stage mainStage;
 
     public MainStage(double minWidth, double minHeight, String title) {
-        makeMainStage(minWidth, minHeight, title);
-    }
-
-    public Stage makeMainStage() {
-        return mainStage;
-    }
-
-    public Stage makeMainStage(double minWidth, double minHeight, String title) {
         mainStage = new Stage();
         mainStage.setMinHeight(minHeight);
         mainStage.setMinWidth(minWidth);
         mainStage.setTitle(title);
+    }
+
+    public Stage getStage() {
         return mainStage;
     }
 }
